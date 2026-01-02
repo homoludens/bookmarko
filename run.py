01c443d2fbb5a0f80015d63c8ecb06f5926a79ed
Marko Djordjevic <marko@droopia.net>
2026-01-02 11:38:13 +0100

    update to flask 3.0

--- run.py
+++ run.py
@@ -1,4 +1,23 @@
-from flaskmarks.core.setup import manager
+"""
+Entry point for running the Flaskmarks application.
+
+Usage:
+    Development: flask run
+    Production: gunicorn 'flaskmarks:create_app()'
+    
+CLI commands:
+    flask create-db     - Create database tables
+    flask drop-db       - Drop database tables  
+    flask create-user   - Create a new user
+    flask list-users    - List all users
+    flask import-marks  - Import bookmarks from file
+    flask db init       - Initialize migrations
+    flask db migrate    - Create migration
+    flask db upgrade    - Apply migrations
+"""
+from flaskmarks import create_app
+
+app = create_app()
 
 if __name__ == '__main__':
-    manager.run()
+    app.run(debug=True)
